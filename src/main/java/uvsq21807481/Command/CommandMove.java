275f744fd41e0b4e5c350a3c0e987db01fd2e023
HERUBEL Hadrien <hadrien.herubel@ens.uvsq.fr>
2020-05-20 15:44:29 +0200

    Command Move

--- src/main/java/uvsq21807481/Command/CommandMove.java
+++ src/main/java/uvsq21807481/Command/CommandMove.java
@@ -1,4 +1,21 @@
 package uvsq21807481.Command;
 
-public class CommandMove implements Command{
+import uvsq21807481.Shape.Shape;
+
+public class CommandMove implements Command {
+
+    private double moveX;
+    private double moveY;
+    private Shape shape;
+
+    public CommandMove(Shape s, double x, double y) {
+        this.shape = s;
+        this.moveX = x;
+        this.moveY = y;
+    }
+
+    @Override
+    public void execute() {
+        this.shape.move(moveX, moveY);
+    }
 }
